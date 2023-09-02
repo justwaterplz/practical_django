635bc0d269bc2c61c756ae5ecd376bdcc97425e2
justwaterplz <102442975+justwaterplz@users.noreply.github.com>
2023-09-02 20:24:07 +0900

    Add files via upload
    ~ 3.29 redirect, reverse

--- challenges/urls.py
+++ challenges/urls.py
@@ -8,6 +8,8 @@ urlpatterns= [
     # path("january", views.january),
     # path("february", views.february),
 
+    #29 added new path
+    path("", views.index), #/challenges/
     #dynamic url pathing using placeholder
 
     #int 동일한 이름의 변수인 month를 사용하는데 str을 사용한 path함수가 먼저 오면 "1"같은 숫자를 str로 받아버리기 때문에
@@ -16,7 +18,7 @@ urlpatterns= [
 
     #path함수 실행 시 challenge/month name 이런 식인데
     #성공시 views.py에 있는 monthly_challenge 함수 실행.
-    path("<str:month>", views.monthly_challenge),
+    path("<str:month>", views.monthly_challenge, name="month-challenge"),
     
     
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
-
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+# reverse
+from django.urls import reverse
 #25. dictionary 추가
+# 각 달의 이름 url(key) - 그에 맞는 값 화면에 출력(value)
 monthly_challenges = {
     "january": "1st view",
     "february": "2nd view",
@@ -29,21 +31,55 @@ monthly_challenges = {
 # def february(request):
 #     return HttpResponse("2nd view")
 
+#29 new view function index
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"    
+    return HttpResponse(response_data)
+
+    # "<li> ... </li><li> ... </li>"    
+# urls.py에 있는 path path("<int:month>", views.monthly_challenge_by_number)를 사용할 때 
+# int형 변수 month를 받기 위해 함수 parameter에 month 추가됨.
+
 def monthly_challenge_by_number(request, month):
-    return HttpResponse(month)
+    # 26. redirect
+    # dictionary의 key에 해당하는 값(달 이름) list로 가짐.
+    #months list의 index = 0 ~ 11(12개)
+    months = list(monthly_challenges.keys())
 
+    #parameter로 받은 month가 invalid함.
+    if month > len(months):
+        return HttpResponseNotFound("invalid month")
+    
+    redirect_month = months[month-1]
 
+    #reverse 함수로 url 생성.
+    redirect_path = reverse("month-challenge", args=[redirect_month]) # challenge/january
+
+    # redirect해서 
+    # return HttpResponseRedirect("/challenge/" + redirect_month)
+    # reverse 사용시
+    return HttpResponseRedirect(redirect_path)
 
 # challenges/urls.py에 path함수 안에 parameter를 <month>라는 dynamic segment로 받았다.
 # 이후 path에서 views.py에 있는 monthly_challenge함수를 호출하는데
 # path에서 사용한 <month> 변수를 받기 위해 parameter가 request말고도 1개 추가된다.
 def monthly_challenge(request, month):
     try:
-        challenge_month = monthly_challenges[month] #페이지 방문시 화면에 표시될 것. 값은 dictionary를 하나 준다.
-        return HttpResponse(challenge_month)
+        challenge_text = monthly_challenges[month] #페이지 방문시 화면에 표시될 것. 값은 dictionary를 하나 준다.
+        # html파일 읽기 f-string으로 h1태그 hardcode함.
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     # try 실패시 responsenotfound
     except:
-        return HttpResponseNotFound("error")
+        return HttpResponseNotFound("<h1>error</h1>")
     # if month == 'january':
     #     challenge_month = "1st view"
     # elif month == 'march':
